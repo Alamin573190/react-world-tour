b3dbaa674881f1a22dc61bfddcdfb598545d6625
Alamin <mdalaminhossain573190@gmail.com>
2024-03-12 13:15:17 +0600

    2nd commoi

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react";
 import Country from "../country/Country";
+import './countries.css'
 
 
 
 
 const Countries = () => {
-    const [countries,setCountries] =useState([]);
-    useEffect(() =>{
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data));
-    } ,[])
+            .then(res => res.json())
+            .then(data => setCountries(data));
+    }, [])
     return (
         <div>
             <h3>Countries:{countries.length}</h3>
-          {
-            countries.map(country =><Country key={country.cca3} country={country}></Country>)
-          }
+            <div  className="countries-containar">
+                {
+                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,12 +1,24 @@
 
+import { useState } from 'react';
 import './Country.css'
 const Country = (country) => {
-    const {name ,flags} = country.country;
-    console.log(country)
+    const {name ,flags ,population ,area ,cca3 } = country.country;
+    const [visited ,setVisited] = useState(false)
+    // console.log(country)
+    const handlevisited = () =>{
+        setVisited(!visited);
+    }
     return (
         <div className='Country'>
             <h3>Name:{name?.common}</h3>
             <img src={flags.png} alt="" />
+
+            <p>population:{population}</p>
+            <p>area:{area}</p>
+            <p>Code:{cca3}</p>
+            {/* <button onClick={handlevisited}>Visited</button> */}
+            <button onClick={handlevisited}>{visited ? 'visited':'going'}</button>
+            {visited?'i have visited this country ': 'I want visit'}
         </div>
     );
 };
